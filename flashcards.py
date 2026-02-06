3acb88c0441f7eb9d21d05c0f32ed63bd01c39e0
kb108109012-sudo <kb108109012@gmail.com>
2026-02-06 07:54:29 +0530

    Added N5 - lessons 2, 3, 4, and 5

--- flashcards.py
+++ flashcards.py
@@ -17,11 +17,32 @@ N5Lesson1 = st.Page(
     icon=":material/book:"
 )
 
+N5Lesson2 = st.Page(
+    page="lessons/N5lesson2.py",
+    title="Lesson 2",
+    icon=":material/book:"
+)
+N5Lesson3 = st.Page(
+    page="lessons/N5lesson3.py",
+    title="Lesson 3",
+    icon=":material/book:"
+)
+N5Lesson4 = st.Page(
+    page="lessons/N5lesson4.py",
+    title="Lesson 4",
+    icon=":material/book:"
+)
+N5Lesson5 = st.Page(
+    page="lessons/N5lesson5.py",
+    title="Lesson 5",
+    icon=":material/book:"
+)
+
 # navigation
 pg = st.navigation(
     {
         "Info": [about_page],
-        "N5" : [N5Lesson1]
+        "N5" : [N5Lesson1, N5Lesson2, N5Lesson3, N5Lesson4, N5Lesson5],
     }
 )
 
---
+++ lessons/N5lesson2.py
@@ -0,0 +1,69 @@
+###### Import packages
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+## to change the size of the button
+st.markdown(
+    """
+<h1 style='text-align: center;'>みんなのにほんご Lesson 2 Vocabulary</h1>
+<style>
+button {
+    height: auto;
+    padding-top: 50px !important;
+    padding-bottom: 50px !important;
+}
+</style>
+
+""",
+    unsafe_allow_html=True,
+)
+
+#### Read vocab list
+df = pd.read_csv("assets/lessonN52.csv", delimiter=",")
+japanese_words = df['Japanese'].to_numpy()
+english_words = df['English'].to_numpy()
+
+st.divider()
+
+n_rows = 7
+n_cols = 7
+
+if 'button_states' not in st.session_state:
+    st.session_state.button_states = {f"btn_{i}": False for i in range(n_rows*n_cols)}
+
+def click_button(button_key):
+    """Callback function to update the button state."""
+    # Toggle the state of the clicked button
+    st.session_state.button_states[button_key] = not st.session_state.button_states[button_key]
+
+dfArray = df.to_numpy()
+
+# display the buttons
+# buttons in a grid structure
+
+
+words = df.shape[0]
+count_word = 0
+for i in range(n_rows):
+    cols = st.columns(n_cols)
+    for j in range(n_cols):
+        button_key = f"btn_{count_word}"
+        with cols[j]:
+            # Change button label/appearance based on state (optional)
+            label = f"{dfArray[i*n_cols+j][1]}" if st.session_state.button_states[button_key] \
+                else f"{dfArray[i*n_cols+j][0]}"
+            st.button(
+                label,
+                key=button_key,
+                on_click=click_button,
+                args=(button_key,),
+                use_container_width=True
+            )
+        count_word = count_word + 1
+        if count_word == words:
+            break
+#st.write("Current button states:", st.session_state.button_states)
+        
+
